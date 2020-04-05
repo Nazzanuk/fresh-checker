348760126d1af3680b28eb09c22747a3193a6eb4
Nathan Nelson <nathan.nelson@acadiasoft.com>
2020-04-05 20:11:13 +0100

    update to guide

--- cypress/integration/amazon/amazon.spec.js
+++ cypress/integration/amazon/amazon.spec.js
@@ -2,7 +2,7 @@ const axios = require("axios").default;
 const moment = require("moment");
 
 const config = require("./../../../fresh-config");
-const cookies = require("../../../data/cookies.json");
+let cookies = require("../../../data/cookies.json") || [];
 
 /** Setup **/
 const tomorrow = moment().add(1, "Day").format("dddd, MMM D");
@@ -23,7 +23,7 @@ describe("Amazon Fresh", function () {
 
     const config = Cypress.config();
 
-    console.log({config})
+    console.log({config});
   });
 
   Cypress._.times(amountToRepeat, (i) => {
@@ -33,38 +33,24 @@ describe("Amazon Fresh", function () {
           cy.setCookie(cookie.name, cookie.value);
         });
 
-
-
         cy.visit(checkoutUrl).then(() => {
-          // cookies.forEach(cookie => {
-          //   cy.setCookie(cookie.name, cookie.value);
-          // });
           cy.getCookies().then(cookies => {
-            console.log("cookies after load", {cookies});
             cy.writeFile("data/cookies.json", cookies);
           });
         });
-
-        // const cookies = cy.readFile("menu.json").then(cookies => {
-          console.log("cookies before", {cookies});
-        // });
-
-
-
-
 
       });
 
       it("Check Today and tomorrow", () => {
         cy.get(".date-button-item:eq(0)").click();
-        // cy.contains("No morning delivery windows are available for Today");
-        // cy.contains("No afternoon delivery windows are available for Today");
-        // cy.contains("No evening delivery windows are available for Today");
-        //
-        // cy.get(".date-button-item:eq(1)").click();
-        // cy.contains(`No morning delivery windows are available for ${tomorrow}`);
-        // cy.contains(`No afternoon delivery windows are available for ${tomorrow}`);
-        // cy.contains(`No evening delivery windows are available for ${tomorrow}x`);
+        cy.contains("No morning delivery windows are available for Today");
+        cy.contains("No afternoon delivery windows are available for Today");
+        cy.contains("No evening delivery windows are available for Today");
+
+        cy.get(".date-button-item:eq(1)").click();
+        cy.contains(`No morning delivery windows are available for ${tomorrow}`);
+        cy.contains(`No afternoon delivery windows are available for ${tomorrow}`);
+        cy.contains(`No evening delivery windows are available for ${tomorrow}x`);
       });
     });
   });
--- readme.md
+++ readme.md
@@ -11,15 +11,18 @@ You'll need node installed along with npm/yarn.
 
 4. Click `run all specs`
     - This will start running the tests
+    ![Run Specs](/images/run-specs.png "Run Specs")
 
 5. Sign in to your account
+    - the first test will fail
     - As cypress is basically an incognito window you might need to do 2 factor auth to sign in
     
 6. Shop / go to checkout screen
     - You need to navigate to the screen where you can choose days and it says "No morning delivery windows are available for Today"
-    ![alt text](/images/checkout.png "Logo Title Text 1")
+    ![Checkout](/images/checkout.png "Checkout")
 
-
-7. Press play
+7. Press refresh
+    ![Refresh](/images/refresh.png "Refresh")
     - That's it, the test will run 1000 times by default at around 5 second per attempt.
-    - The screen will stop
+    - It will check dates for today and tomorrow
+    - The screen will stop when the test fails (i.e. a slot is available)

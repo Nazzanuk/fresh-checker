fdf766bcd0a7e46637ab10f74aa85d861282e0cd
Adrian Nelson <anel1989@googlemail.com>
2020-04-29 20:09:54 +0100

    Add random sleeps and include the day after tomorrow in the search (#1)

--- cypress/integration/amazon/amazon.spec.js
+++ cypress/integration/amazon/amazon.spec.js
@@ -6,6 +6,7 @@ let cookies = require("../../../data/cookies.json") || [];
 
 /** Setup **/
 const tomorrow = moment().add(1, "Day").format("dddd, MMM D");
+const dayAfterTomorrow = moment().add(2, "Day").format("dddd, MMM D");
 const checkoutUrl = "https://www.amazon.co.uk/gp/buy/shipoptionselect/handlers/display.html?hasWorkingJavascript=1";
 
 const amountToRepeat = config.amountToRepeat;
@@ -46,15 +47,28 @@ describe("Amazon Fresh", function () {
       });
 
       it("Check Today and tomorrow", () => {
+        const time1 = (Math.floor(Math.random() * 50) + 2) * 100;
+        const time2 = (Math.floor(Math.random() * 50) + 2) * 100;
+        const time3 = (Math.floor(Math.random() * 250) + 10) * 100;
+        const time4 = (Math.floor(Math.random() * 250) + 10) * 100;
+        cy.wait(time3);
         cy.get(".date-button-item:eq(0)").click();
         cy.contains("No morning delivery windows are available for Today");
         cy.contains("No afternoon delivery windows are available for Today");
+        cy.wait(time1);
         cy.contains("No evening delivery windows are available for Today");
 
         cy.get(".date-button-item:eq(1)").click();
         cy.contains(`No morning delivery windows are available for ${tomorrow}`);
         cy.contains(`No afternoon delivery windows are available for ${tomorrow}`);
+        cy.wait(time2);
         cy.contains(`No evening delivery windows are available for ${tomorrow}`);
+
+        cy.get(".date-button-item:eq(2)").click();
+        cy.contains(`No morning delivery windows are available for ${dayAfterTomorrow}`);
+        cy.contains(`No afternoon delivery windows are available for ${dayAfterTomorrow}`);
+        cy.wait(time4);
+        cy.contains(`No evening delivery windows are available for ${dayAfterTomorrow}`);
       });
     });
   });
